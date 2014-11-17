572ac86846cc0bbde8681f7b7637cac3cd62a3ef
Bryan Richter <b@chreekat.net>
2014-11-17 22:26:51 +0100

    Update publish script to do a thing

--- traditional/publish.sh
+++ traditional/publish.sh
@@ -1,1 +1,28 @@
-cp -r boardCell.html bower_components fladdermus.js gameBoard.js hiScores.html img index.html smiley.html uncover.js util.js ../Hangular
+#!/bin/bash
+set -e
+
+files=(
+    boardCell.html
+    bower_components
+    fladdermus.js
+    gameBoard.js
+    hiScores.html
+    img
+    index.html
+    smiley.html
+    uncover.js
+    util.js
+)
+
+cd $(git root)
+git stash
+
+tmp=$(mktemp -d)
+trap "rm -rf $tmp" EXIT
+
+cp -r traditional $tmp
+git checkout gh-pages
+gitdir=$PWD
+
+cd $tmp/traditional
+cp -r ${files[@]} $gitdir
