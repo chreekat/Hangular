616ee06d1a2a24e6aaa4430dd85ff5832b9473c2
Bryan Richter <b@chreekat.net>
2014-12-02 14:30:22 +0100

    Add and configure grunt/grunt-jslint

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jslint: {
+            all: {
+                src: [
+                    "fladdermus.js",
+                    "util.js",
+                    "gameBoard.js",
+                ]
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-jslint');
+
+    // Default task(s).
+    grunt.registerTask('default', ['jslint']);
+
+};
